79a8e1ff1dfd581ba09c21fa5fd1d7fae7d7d31d
thomaschang710114 <yachih_chang@yahoo.com.tw>
2026-09-09 12:04:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -406,7 +406,7 @@ async def trigger_error(request):
 
 routes = [
     # ⚠️ 把自訂 healthz 放在 routes 清單最前面
-    Route("/healthz", custom_healthz),
+    # Route("/healthz", custom_healthz),
     # Section 1: Custom Routes & Static
     Route("/api/raw-data", custom_starlette_data),
     Route("/api/html-demo", html_response_demo),
